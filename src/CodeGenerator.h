d0a4e13c7bfef27c6f44bce382199843dcc750be
Kiril Abadjiev <52859306+boxDev2008@users.noreply.github.com>
2025-12-27 18:23:00 +0200

    function pointer safety

--- src/CodeGenerator.h
+++ src/CodeGenerator.h
@@ -443,10 +443,35 @@ private:
         
         Type* fromLLVMType = fromType.llvmType;
         Type* toLLVMType = toType.llvmType;
+
+        if ((toType.functionInfo && !fromType.functionInfo))
+            throw std::runtime_error("Cannot assign non-function pointer to function pointer");
         
         // Pointer to Pointer
         if (fromLLVMType->isPointerTy() && toLLVMType->isPointerTy())
         {
+            if (toType.functionInfo)
+            {
+                if (!fromType.functionInfo)
+                    throw std::runtime_error("Cannot assign non-function pointer to function pointer in constant expression");
+                
+                const FunctionInfo& srcFunc = *fromType.functionInfo;
+                const FunctionInfo& dstFunc = *toType.functionInfo;
+                
+                if (srcFunc.returnType != dstFunc.returnType)
+                    throw std::runtime_error("Function pointer return type mismatch in constant expression");
+                
+                if (srcFunc.paramTypes.size() != dstFunc.paramTypes.size())
+                    throw std::runtime_error("Function pointer parameter count mismatch in constant expression");
+                
+                for (size_t i = 0; i < srcFunc.paramTypes.size(); i++)
+                {
+                    if (srcFunc.paramTypes[i] != dstFunc.paramTypes[i])
+                        throw std::runtime_error("Function pointer parameter type mismatch in constant expression");
+                }
+                
+                return value;
+            }
             if (fromType.pointeeType == toType.pointeeType)
                 return value;
             return ConstantExpr::getBitCast(value, toLLVMType);
@@ -1573,6 +1598,9 @@ private:
         
         Type* fromType = value.type.llvmType;
         Type* toType = targetType.llvmType;
+
+        if ((targetType.functionInfo && !value.type.functionInfo))
+            throw std::runtime_error("Cannot assign non-function pointer to function pointer");
         
         // Pointer to Pointer
         if (fromType->isPointerTy() && toType->isPointerTy())
